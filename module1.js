0a94dd9aad1d201577a8059152bc6d2a3bbff9c3
nico1988 <410827024@qq.com>
2018-06-29 21:12:51 +0800

    parcel demo

---
+++ module1.js
@@ -0,0 +1,14 @@
+// import "./module2.js";
+// console.log(1);
+// const values = {a:1,b:2}
+// const newvalues ={
+//   ...values,
+//   c:3
+// }
+// console.log(newvalues);
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<h1>hello parcel!</h1>,
+  document.getElementById('app')
+)
